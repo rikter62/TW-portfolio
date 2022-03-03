8ef80569f651525e2b865cbe01dad10db8601eb9
RICHARD GROVES <rikter62@gmail.com>
2022-03-02 21:03:27 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'body': '#fffaf3',
+        'selected-text': '#fffaf3',
+        'theme': '#f47555',
+        'nav': '#efdedd',
+        'secondary': '#3c0d01',
+        'input-border': '#E8E8E8',
+        'input': '#efefef'
+      },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+        'ranchers': ['Ranchers', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
